51fa4e856190b23122d6297a1f437fcfaef7e2df
thealicat13 <alicia.baker13@gmail.com>
2017-02-11 23:39:00 -0800

    Add set score button, add header with game instruction and win score, update general styling

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 		document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
 		// check for win before before switching players if there is no win
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= 100) {
 			document.getElementById('name-' + activePlayer).textContent = 'Winner!';
 			// activate winner class to style the 'Winner!' text
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
